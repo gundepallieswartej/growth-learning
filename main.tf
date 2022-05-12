f9ed5ce364f65d27b29166b7a6d79b55e4ff886d
gundepallieswartej <104007319+gundepallieswartej@users.noreply.github.com>
2022-05-12 15:21:25 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,22 @@
+provider "aws" {
+    access_key = "${var.aws_access_key}"
+    secret_key = "${var.aws_secret_key}"
+    region = "${var.aws_region}"
+}
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+}
+resource "aws_vpc" "default" {
+    cidr_block = "${var.vpc_cidr}"
+    enable_dns_hostnames = true
+    tags = {
+        Name = "${var.vpc_name}"
+        Owner = "gundepalli eswar"
+        environment = "${var.environment}"
+    }
+}
